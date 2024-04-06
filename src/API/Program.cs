2b74077433000f16ef626ce1bdab9475554fe28c
Caio Henrique Molinaro Soares <cmolinaro19@gmail.com>
2024-04-06 14:43:53 -0300

    feat: Changed API name in Swagger

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 using Notepad.API.DependencyInjection;
 using Notepad.API.Shared.Data;
 using Notepad.API.Shared.Middlewares;
@@ -7,7 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Notepad.API", Version = "v1" });
+});
 
 builder.Logging.ClearProviders();
 builder.Services.AddCarter();
